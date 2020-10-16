f9692920950b5c2dfe2cbdba00d07eff62b61127
Ruth Ironside <ruthironside@live.com>
2020-10-16 17:59:03 +0100

    problem .5

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,3 +5,12 @@ def get_pet_shop_name(petshop):
 # 2.
 def get_total_cash(money):
     return money ["admin"]["total_cash"]
+
+# 3.
+    
+
+# 4.
+
+# 5. 
+def get_pets_sold(sold):
+    return sold ["admin"]["pets_sold"]
